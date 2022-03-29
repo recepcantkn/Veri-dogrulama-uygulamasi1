0322180d0403bfd4e06d73850615e2964f1be470
Recep Can <61096668+recepcantkn@users.noreply.github.com>
2022-03-29 08:58:28 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtAd.Text))
+            {
+                MessageBox.Show("Kullanıcı adını giriniz.", "Uyarı" );
+            }
+            if (string.IsNullOrEmpty(txtSifre.Text))
+            {
+                MessageBox.Show("Şifreyi giriniz.", "Uyarı");
+
+            }
+        }
+    }
+}
